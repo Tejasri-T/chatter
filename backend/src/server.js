f8c2f6f2a6470927b39c77a9f66a7c0912b22a82
Tejasri Teyam <tsree0421@gmail.com>
2026-03-07 20:54:58 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 
 import authRoutes from './routes/auth.route.js';
@@ -9,8 +10,18 @@ import messageRoutes from './routes/message.route.js';
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
+
+//make ready for production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('*', (_, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    });
+}
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
