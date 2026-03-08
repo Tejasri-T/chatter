f1134d6b25da4a7428c64b4e7bff6c11549eb180
Tejasri Teyam <tsree0421@gmail.com>
2026-03-08 18:33:24 +0530

    login and logout functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -65,10 +65,38 @@ export const signup = async (req, res) => {
     }
 };
 
-export const login =(req, res) => {
-    res.send('login route');
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try{
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Email and password are required' });
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+        const isPasswordCOrrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCOrrect) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePicture: user.profilePicture
+        });
+
+    }catch(error){
+        console.error('Error during login:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+    
 };
 
-export const logout =(req, res) => {
-    res.send('logout route');
+export const logout =(_, res) => {
+    res.cookie("jwt","",{maxAge:0})
+    res.status(200).json({message:'Logged out successfully'})
 };
