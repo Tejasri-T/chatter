0e85f98fbb522f033b617c417b6164ee2ed99a21
Tejasri Teyam <tsree0421@gmail.com>
2026-03-17 18:29:17 +0530

    made signup page

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,12 +1,37 @@
 import {create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
-    authUser: {name: "John Doe", _id: "12345"},
-    isLoading: false,
-    isLoggedIn: false,
+    authUser: null,
+    isCheckingAuth: true,
+    isSigningUp: false,
 
-    login: () => {
-        console.log("Logging in...");
-        set({isLoggedIn : true})
+    signUp: async (data) => {
+        set({ isSigningUp: true });
+        try {
+
+            const res = await axiosInstance.post("/auth/signup", data);
+            set({ authUser: res.data });
+
+            //toast 
+            toast.success("Signup successful! Welcome to Chatter.");
+
+        }catch (error) {
+            console.error("Error signing up", error);
+            toast.error("Error signing up. Please try again."); 
+        }finally {
+            set({ isSigningUp: false });
+        }
     },
+
+    checkAuth: async () => {
+        try{
+            const res = await axiosInstance.get("/auth/check");
+            set({ authUser: res.data, isCheckingAuth: false });
+        }catch(error){
+            console.error("Error checking auth status", error);
+            set({ authUser: null, isCheckingAuth: false });
+        }
+    }
 }));
